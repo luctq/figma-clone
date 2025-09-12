df1e545dc24aaf96831bb50e21fb1f0d3e90be2f
luctq-3361 <tran.quoc.luc@sun-asterisk.com>
2025-09-13 02:12:29 +0900

    feature: add base lib, public, types, constants

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { Room } from "./Room";
-import { CollaborativeApp } from "./CollaborativeApp";
-
-export default function Page() {
+export default function Home() {
   return (
-    <Room>
-      <CollaborativeApp />
-    </Room>
-  );
+    <div className="h-[100vh] w-full flex justify-center items-center text-white">
+      <h1 className="text-2xl text-white">Liveblocks Figma Clone</h1>
+    </div>
+  )
 }
--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -6,21 +6,18 @@ declare global {
     Presence: {
       // Example, real-time cursor coordinates
       // cursor: { x: number; y: number };
-      cursor: { x: number; y: number };
     };
 
     // The Storage tree for the room, for useMutation, useStorage, etc.
     Storage: {
       // Example, a conflict-free list
       // animals: LiveList<string>;
-      type: "TEMP";
     };
 
     // Custom user info set when authenticating with a secret key
     UserMeta: {
       id: string;
       info: {
-        type: "TEMP";
         // Example properties, for useSelf, useUser, useOthers, etc.
         // name: string;
         // avatar: string;
@@ -28,9 +25,7 @@ declare global {
     };
 
     // Custom events, for useBroadcastEvent, useEventListener
-    RoomEvent: {
-      type: "TEMP";
-    };
+    RoomEvent: {};
       // Example has two events, using a union
       // | { type: "PLAY" } 
       // | { type: "REACTION"; emoji: "🔥" };
@@ -40,7 +35,6 @@ declare global {
       // Example, attaching coordinates to a thread
       // x: number;
       // y: number;
-      type: "TEMP";
     };
 
     // Custom room info set with resolveRoomsInfo, for useRoomInfo
@@ -48,7 +42,6 @@ declare global {
       // Example, rooms with a title and url
       // title: string;
       // url: string;
-      type: "TEMP";
     };
   }
 }
